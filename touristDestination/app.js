189003152349193558c0de71f549ca5296c28e2d
Nícolas <nicolasbarcellos95@hotmail.com>
2018-02-05 23:30:05 -0200

    tourist destination

---
+++ touristDestination/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.main-carousel').slick({   
+    dots: false,
+    infinite: true,
+    speed: 300,
+    cssEase: 'linear',
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
+
